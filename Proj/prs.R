2dad67689ceedee62aad2bee665cba33d3f4dd23
Ember404 <96316086+Ember404@users.noreply.github.com>
2023-01-26 21:03:12 +0100

    added PRS

---
+++ Proj/prs.R
@@ -0,0 +1,15 @@
+minUsingPRS<-function(fn_raw, nRepeats, points){
+  params = attr(fn_raw, "par.set")$pars$x
+  lowerBound = params$lower
+  upperBound = params$upper
+  nDimensions = params$len
+  
+  res_array=replicate(n = nRepeats,
+                      {pom = array(c(runif(points,lowerBound,upperBound)),dim = c(points,nDimensions))
+                      min = fn_raw(c(pom[1,]))
+                      for (i in range(2:1000)) {
+                        if (fn_raw(c(pom[i,])) < min)
+                        {min = fn_raw(c(pom[i,]))}
+                      }
+                      min})
+  return(res_array)}
